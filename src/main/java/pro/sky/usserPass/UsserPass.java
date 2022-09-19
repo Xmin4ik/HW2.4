ef73c35cc8b978791fdab215ff4ab71395409f13
xmin3 <Xmin.34@gmail.com>
2022-09-19 21:22:57 +0300

    commit 2.4

---
+++ src/main/java/pro/sky/usserPass/UsserPass.java
@@ -0,0 +1,13 @@
+package pro.sky.metodcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsserPass {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsserPass.class, args);
+	}
+
+}
