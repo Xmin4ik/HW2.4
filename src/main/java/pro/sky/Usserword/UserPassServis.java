233d29e743379d7d0e78b7b473239d6404253faa
xmin3 <Xmin.34@gmail.com>
2022-09-23 17:12:39 +0300

    commit 2.4.1HW

--- src/main/java/pro/sky/Usserword/UserPassServis.java
+++ src/main/java/pro/sky/Usserword/UserPassServis.java
@@ -2,65 +2,50 @@ package pro.sky.Usserword;
 
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-
 @Service
 public class UserPassServis implements UserPassInterface {
     public String hello() {
         return "<b>hello world</b>";
     }
 
-    public String tryLogin(String login, String password, String confirmPassword) {
-        final UserPassServis userPassServis;
-        char[] a = {'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '_', '~',};
-        boolean result1 = false;
-        boolean result2 = false;
-        String data=" ";
-        if (login.length() <= 20 && password.length() < 20 && Arrays.equals(password.toCharArray(), confirmPassword.toCharArray())) {
-            for (int i = 0; i < login.length(); i++) {
-                for (int j = 0; j < a.length; j++) {
-                    if (login.charAt(i) == a[j]) {
-                        result1 = true;
-                        System.out.println(login.charAt(i) + " " + a[j]);
-                        break;
-                    } else if (a.length - 1 <= j && login.charAt(i) != a[j]) {
-                        System.out.println("login not pass");
-                        result1 = false;
-                        break;
-                    }
-                }
-                if (!result1) {
-                    System.out.println("login not pass!!!");
-                    break;
-                }
-            }
-            for (int i = 0; i < password.length(); i++) {
-                for (int j = 0; j < a.length; j++) {
-                    if (password.charAt(i) == a[j]) {
-                        result2 = true;
-                        System.out.println(password.charAt(i) + " " + a[j]);
-                        break;
-                    } else if (a.length - 1 <= j && password.charAt(i) != a[j]) {
-                        System.out.println("password  not pravilno");
-                        result2 = false;
-                        break;
-                    }
-                }
-                if (!result2) {
-                    System.out.println("login not pass!!!");
-                    break;
+    private static final String symbol = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789._~";
+
+    public boolean tryLogin(String login, String password, String confirmPassword) {
+        try {
+            chekLogAndPass(login, password, confirmPassword);
+        } catch (WrongLoginException | WrongPasswordException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static void chekLogAndPass(String login, String password, String confirmPassword
+
+    ) throws WrongLoginException, WrongPasswordException {
+
+        if (login == null || login.length() > 20) {
+            throw new WrongLoginException("Длина логина привышает допустимые значения");
+        }
+        if (password == null || password.length() >= 20) {
+            throw new WrongLoginException("Длина пароля привышает допустимые значения");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Пароль не подтвержден");
+        }
+        chekSymbols(login, true);
+    }
+
+    private static void chekSymbols(String insert, boolean isLogin) throws WrongLoginException, WrongPasswordException {
+        // Я в начале так думал сделать, но у меня в голове как это сделать не составилось  	(｡╯︵╰｡)
+        for (int i = 0; i < insert.length(); i++) {
+            if (symbol.contains(String.valueOf(insert))) {
+                if (isLogin) {
+                    throw new WrongLoginException("Использован недопустимый символ - " + insert.charAt(i));
+                } else {
+                    throw new WrongPasswordException("Использован недопустимый символ - " + insert.charAt(i));
                 }
             }
-            if (!result1 && !result2) {
-                System.out.println("login and pass");
-                data = "Данные введены не корректно";
-            }if (result1 && result2) {
-                System.out.println("login and pass");
-                data = "Данные введены верно";
-            }
-            System.out.println(result1 + " " + result2);
 
         }
-
-        return data;
-    }}
+    }
+}
